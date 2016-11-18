a5e20b8389202a40e122e60ef6c4cdffc1bdc9f0
Ricard Ejarque Montalvo <Ricard Ejarque Montalvo>
2016-11-18 08:25:55 +0100

    Initial commit

---
+++ app/src/main/java/app/com/example/ricard/sunshine/MainActivity.java
@@ -0,0 +1,69 @@
+package app.com.example.ricard.sunshine;
+
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new forecastFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction =  fragmentManager.beginTransaction();
+            transaction.replace(R.id.container, new settingsFragment());
+            transaction.addToBackStack(null);
+            transaction.commit();
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            Log.v("aa","aa");
+            return true;
+        }
+
+        if (id == android.R.id.home){
+            onBackPressed();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onBackPressed(){
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        super.onBackPressed();
+    }
+
+
+
+}
---
+++ app/src/main/java/app/com/example/ricard/sunshine/settingsFragment.java
@@ -0,0 +1,45 @@
+package app.com.example.ricard.sunshine;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * Created by Ricard on 17/11/2016.
+ */
+
+public class settingsFragment extends Fragment {
+
+    public settingsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.settings_main, container, false);
+        return rootView;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add this line in order for this fragment to handle menu events.
+        setHasOptionsMenu(true);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
